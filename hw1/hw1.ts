f228e379de09c625aecbe1902e27a047dc6b9cb0
Anya-Kirshteyn <ann65ann@gmail.com>
2025-05-01 12:41:11 +0300

    tsc hw1 pt3

--- hw1/hw1.ts
+++ hw1/hw1.ts
@@ -211,34 +211,137 @@
 
 // <!--За допомоги іф перевірити кожен його елемент на тривалість навчання.-->
 // <!--У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".-->
-type Course={ title:string,monthDuration:number }
-    let coursesAndDurationArray:Course[] = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-if (coursesAndDurationArray[0].monthDuration > 5){
-    console.log("super")}
-if (coursesAndDurationArray[1].monthDuration > 5){
-    console.log("super")}
-if (coursesAndDurationArray[2].monthDuration > 5){
-    console.log("super")}
-if (coursesAndDurationArray[3].monthDuration > 5){
-    console.log("super")}
-if (coursesAndDurationArray[4].monthDuration > 5){
-    console.log("super")}
-if (coursesAndDurationArray[5].monthDuration > 5){
-    console.log("super")}
+// type Course={ title:string,monthDuration:number }
+//     let coursesAndDurationArray:Course[] = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// if (coursesAndDurationArray[0].monthDuration > 5){
+//     console.log("super")}
+// if (coursesAndDurationArray[1].monthDuration > 5){
+//     console.log("super")}
+// if (coursesAndDurationArray[2].monthDuration > 5){
+//     console.log("super")}
+// if (coursesAndDurationArray[3].monthDuration > 5){
+//     console.log("super")}
+// if (coursesAndDurationArray[4].monthDuration > 5){
+//     console.log("super")}
+// if (coursesAndDurationArray[5].monthDuration > 5){
+//     console.log("super")}
 
+// ====================================================================================================================================
+// ====================================================================================================================================
+// ====================================================================================================================================
+// ====================================================================================================================================
+//
+// --------------------hw3.3--------------------
+//     За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i:number=0;
+// while(i<20){
+//     document.write('<h1> AAAAAAAAAAA</h1>');
+//     i++;
+// }
+// / ---------------------hw3.4---------------------
+//     - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+// let i:number=0;
+// while (i < 20) {
+//     document.write(`<h1>sdfsdfsdfsdf ${i}</h1>`);
+//     i++;
+// }
+// --------------------hw3.5-------------------------
+//     - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+//
+//     let listOfItems:string[] = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// document.write(`<ul>`);
+// for( let item of listOfItems){
+//
+//     document.write(` <li> ${item} </li> `);
+//
+//
+// }
+// document.write(`</ul>`);
 
-
-
-
-
-
-
-
+// ------------------hw3.6-------------------
+//     Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+// ШАБЛОН
+// <div class="product-card">
+//     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+// type Product={
+//     title: string,
+//         price:number,
+//         image: string
+// }
+//
+//     let products: Product= [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+// for(let product of products){
+//     document.write(`
+//         <div class="product-card">
+//             <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+//             <img src="${product.image}" alt="" class="product-image"/>
+//         </div>
+//     `)
+//
+//
+// }
+// / --------------------hw3.7------------------------
+// //     за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+//
+// type User={
+//     name:string,
+//     age: number,
+//     status: boolean
+// }
+//     let users:User[] = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ]
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
+//     hw 4.1
 
