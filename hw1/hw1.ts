92d7f4f86fba6e24ae9b86a56d201fd09c044838
Anya-Kirshteyn <ann65ann@gmail.com>
2025-05-01 15:02:41 +0300

    tsc hw1 pt4-5

--- hw1/hw1.ts
+++ hw1/hw1.ts
@@ -345,3 +345,299 @@
 // ===================================================================
 //     hw 4.1
 
+// створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// function upright(a:number,b:number){
+//     return a*b;
+// }
+// створити функцію яка обчислює та повертає площу кола з радіусом r
+// function circle(radius:number) {
+//     return 3.14*radius*radius;
+// }
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+// function  cylinder(radius:number, height:number) {
+//     return 2*3.14*radius*(radius+height);
+// }
+// console.log(cylinder(10,15));
+// створити функцію яка приймає масив та виводить кожен його елемент
+// function printer<T>(arrays:T[]) {
+//     for( let array of arrays ) {
+//         console.log(array);
+//     }
+// }
+// створити функцію яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+// function writeParagraph(text: string):void {
+//     document.write(`
+//         <p>
+//         ${text}
+//         </p>`
+//     )
+//
+// }
+// створити функцію яка створює ul з трьома елементами li та виводить його через document.write.
+//     Текст li задати через аргумент всім однаковий
+// function triLi(textLi:string):void {
+//     document.write(
+//         `
+//        <ul>
+//        <li>${textLi}</li>
+//        <li>${textLi}</li>
+//        <li>${textLi}</li>
+//
+//        </ul>
+//         `
+//     )
+// }
+// triLi("sobaka")
+// - створити функцію яка створює ul з  елементами li. Текст li задати через аргумент всім однаковий.
+//     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл) та виводить його через document.write
+// function somename(textLi:string, print:number):void {
+//
+//     document.write("<ul>")
+//
+// for( let i = 0; i < print; i++){
+//
+//     document.write(`<li>${textLi}</li>`)
+// }
+//
+//     document.write("</ul>")
+// }
+// somename('gusi', 5);
+// створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві),
+//     та будує для них список (ul li) та виводить його через document.write
+//
+// function createList(array:(string | number | boolean)[]):void{
+//     document.write("<ul>")
+//
+//     for(let item of array){
+//         document.write(`<li>${String(item)}</li>`)
+//     }
+//     document.write("</ul>")
+//
+// }
+// createList([12, true, 'pompa',13, 31])
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age ,' +
+// ' та виводить їх в документ. Для кожного об'єкту окремий блок.
+// type User={
+//     id:number,
+//     name:string,
+//     age:number
+// }
+// function usercount(users:User[]):void {
+//     for (let user of users) {
+//         document.write(
+//             `
+//         <div>
+//         <p>${user.id}</p>
+//         <p>${user.name}</p>
+//         <p>${user.age}</p>
+//        </div>
+//         `)
+//     }
+// }
+// const usersArray:User[] = [
+//     { id: 1, name: "Анна", age: 25 },
+//     { id: 2, name: "Иван", age: 30 },
+//     { id: 3, name: "Олег", age: 22 }
+// ];
+// usercount(usersArray);
+// // / - створити функцію яка повертає найменьше число з масиву
+// function minValue(numbers:number[]):number{
+//     let min=numbers[0];
+//     for (let number of numbers){
+//         if(number<min){min=number;}
+//     }
+//     return min;
+// }
+// console.log(minValue([80,2,3,4,5,6]));
+// - створити функцію sum(arr) яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+//     Приклад sum([1,2,10]) //->13
+// function sum(arrays:number[]):number {
+//     let suma:number = 0;
+//     for (let array of arrays) {
+//         suma=suma+array;
+//     }
+//     return suma;
+// }
+// console.log(sum([1,2,10]));
+// // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
+// // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+//
+// function swap(arr:(number|string|boolean)[], index1:number, index2:number):(number|string|boolean)[]{
+// let temp = arr[index1];
+//     arr[index1] = arr[index2];
+//     arr[index2] = temp;
+//     return arr;
+// }
+// console.log(swap([11, 22, 33, 44], 0, 1));
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
+// type CurVal={
+//     currency:string,
+//     value:number
+// }
+// function exchange(sumUAH:number,currencyValues:CurVal[],exchangeCurrency:string):number |string{
+//     for(let cheker of currencyValues){
+//         if(cheker.currency === exchangeCurrency){
+//             return sumUAH/cheker.value;
+//         }
+//     }
+//     return 'currency not found';
+// }
+// console.log(exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD'));
+// ===========================================================================
+// ============================================================================
+//     =========================================================================
+// ========================================================================
+// hw 5.1
+// створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// const upright=(a:number, b:number):number => a*b;
+// console.log(upright(11,22));
+// 5.2
+// створити функцію яка обчислює та повертає площу кола з радіусом r
+// const circle=(radius:number):number=>Math.PI*radius*radius;
+// 5.3
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+//
+// const cylinder=(radius:number, height:number):number=>2*3.14*radius*(radius+height)
+// console.log(cylinder(10,15))
+// 5.4
+// // створити функцію яка приймає масив та виводить кожен його елемент
+// const printer = (array:(string |number | boolean )[]):void => {
+//     array.forEach(element => console.log(element));
+// };
+// printer([1, 22, 33, 42]);
+// 5.5
+// / створити функцію яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+//
+// const writeParagraph=(text:string):void=>{
+//
+//     document.write(`
+//         <p>
+//         ${text}
+//         </p>`
+//     )
+//
+// }
+// writeParagraph('Hello World!')
+// 5.6
+// створити функцію яка створює ul з трьома елементами li та виводить його через document.write.
+//     Текст li задати через аргумент всім однаковий
+// const triLi=(textLi:string):void=>{document.write(
+//     `
+//          <ul>
+//        <li>${textLi}</li>
+//        <li>${textLi}</li>
+//        <li>${textLi}</li>
+//        </ul>
+//
+//     `)
+// }
+// triLi("sobaka")
+// 5.7
+// - створити функцію яка створює ul з  елементами li. Текст li задати через аргумент всім однаковий.
+//     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл) та виводить його через document.write
+//  const somename=(textLi:string, print:number):void=>{
+//      document.write("<ul>")
+//
+//      for (let i = 0; i < print; i++) {
+//          document.write(`<li>${textLi}</li>`);
+//      }
+//
+//      document.write("</ul>")
+//
+//  }
+// somename('gusi', 5);
+// 5.8
+// створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві),
+//     та будує для них список (ul li) та виводить його через document.write
+// const createList=(array:(string|number|boolean)[]):void=>{
+//     document.write("<ul>")
+//     array.forEach(element => {
+//         document.write(`<li>${String(element)}</li>`)
+//     })
+//
+//     document.write("</ul>")
+//
+// }
+// createList([12, true, 'pompa',13, 31])
+// 5.9
+// type User={
+//     id:number,
+//     name:string,
+//     age:number
+// }
+// const usercount=(users:User[]):void=>{
+//     users.forEach(user=>{
+//         document.write(
+//             `
+//         <div>
+//         <p>${user.id}</p>
+//         <p>${user.name}</p>
+//         <p>${user.age}</p>
+//        </div>`)})
+// }
+//
+// const usersArray:User[] = [
+//     { id: 1, name: "Анна", age: 25 },
+//     { id: 2, name: "Иван", age: 30 },
+//     { id: 3, name: "Олег", age: 22 }
+// ];
+// usercount(usersArray);
+// 5.10
+// - створити функцію яка повертає найменьше число з масиву
+// const minValue=(numbers:number[]): number=>{
+//     let min=numbers[0];
+//     numbers.forEach(number=>{
+//         if(number<min){
+//             min=number;
+//         }
+//     })
+//     return min;
+// }
+// console.log(minValue([80,50,38,4,5,6]));
+// 5.11
+// - створити функцію sum(arr) яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+//     Приклад sum([1,2,10]) //->13
+// const sum=(arrays:number[]):number=>{
+//     let suma = 0;
+//     arrays.forEach(array=>{
+//         suma=suma+array;
+//     })
+//     return suma;
+// }
+// console.log(sum([1,2,10]));
+// 5.12
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+// const swap=(arr:number[], index1:number, index2:number):number[]=>{
+//     let temp = arr[index1];
+//     arr[index1] = arr[index2];
+//     arr[index2] = temp;
+//     return arr;
+//
+// }
+// console.log(swap([11, 22, 33, 44], 0, 1));
+// 5.13
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
+// type CurVal={
+//     currency: string,
+//     value: number
+// }
+// const exchange = (sumUAH:number, currencyValues:CurVal[], exchangeCurrency:string):'currency not found'|number => {
+//     for (let cheker of currencyValues) {
+//         if (cheker.currency === exchangeCurrency) {
+//             return sumUAH / cheker.value;
+//         }
+//     }
+//     return 'currency not found';
+// };
+//
+// console.log(exchange(10000, [{ currency: 'USD', value: 25 }, { currency: 'EUR', value: 42 }], 'USD'));
+// ===========================================================================
+// ===========================================================================
+// ===========================================================================
+// ===========================================================================
+// ===========================================================================
+
+// hw6.1
